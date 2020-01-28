b6d6ede7a4ef98288e75992fdf0343fbbb6026d9
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-28 15:37:21 -0600

    CAP-309 src/components/login/login.component.ts update the LogIn component.

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,7 +1,6 @@
-import { Component, ViewEncapsulation, Inject, PLATFORM_ID  } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormGroup, FormControl, Validators, } from '@angular/forms';
 import { AuthenticationService } from '../../services/authentication.service';
-import { isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
@@ -20,34 +19,40 @@ import { Router } from '@angular/router';
                     <input  type="text"
                             class="form-control"
                             placeholder="Email address *"
-                            [ngClass]="{'is-invalid':!loginUserForm.get('email').valid && loginUserForm.get('email').touched}"
+                            [ngClass]="{
+                              'invalidField':(!loginUserForm.get('email').valid
+                              && loginUserForm.get('email').touched)
+                              || (validatedForm && !loginUserForm.get('email').valid)}"
                             formControlName="email"/>
                 </div>
                 <div class="form-group">
                     <input  type="password"
                             class="form-control"
                             placeholder="Password *"
-                            [ngClass]="{'is-invalid':!loginUserForm.get('password').valid && loginUserForm.get('password').touched}"
+                            [ngClass]="{
+                              'invalidField':(!loginUserForm.get('password').valid
+                              && loginUserForm.get('password').touched)
+                              || (validatedForm && !loginUserForm.get('password').valid)}"
                             formControlName="password"/>
                     <small *ngIf="!loginUserForm.get('password').valid && loginUserForm.get('password').touched" class="form-text text-center text-muted">
                       Your password must be 8-20 characters long, contain letters and numbers and the first letter has to be uppercase.
                     </small>
 
                   <div *ngIf="userNotValid"  class="form-control-feeback text-danger text-center">
-                      This user already exists, try other alternate data
+                    invalid email or password
+                  </div>
+                  <div *ngIf="socialMedia"  class="form-control-feeback text-danger text-center">
+                    At the moment authentication with Social networks is under development, try by Email
                   </div>
               </div>
             </div>
               <div class="col-md-6">
                 <div class="form-group">
-                  <button type="submit"
-                          class="btnSubmit"
-                          [disabled]="!loginUserForm.valid">Email</button>
+                  <button type="submit"class="btnSubmit">Login</button>
                 </div>
                 <div class="form-group">
                   <button (click)="signInSocialMedia(true)" type="button" class="btnFacebook ">Facebook</button>
                 </div>
-
                 <div class="form-group">
                 <button (click)="signInSocialMedia(false)" type="button" class="btnGoogle ">Google</button>
               </div>
@@ -60,127 +65,133 @@ import { Router } from '@angular/router';
 
   `,
   styles: [`
-.header
-{
-  text-align: center;
-  height: 80px;
-  background: black;
-  color: #fff;
-  font-weight: bold;
-  line-height: 80px;
-}
-.form-content
-{
-  padding: 5%;
-  border: 1px solid #ced4da;
-  margin-bottom: 2%;
-}
-.form-control{
-  border-radius:1.5rem;
-}
-.btnSubmit
-{
-  border:none;
-  border-radius:1.5rem;
-  padding: 1%;
-  width: 100%;
-  cursor: pointer;
-  background: black;
-  color: #fff;
-}
-.btnFacebook
-{
-  border:none;
-  border-radius:1.5rem;
-  padding: 1%;
-  width: 100%;
-  cursor: pointer;
-  background: #0000FF;
-  color: #fff;
-}
-.btnGoogle
-{
-  border:none;
-  border-radius:1.5rem;
-  padding: 1%;
-  width: 100%;
-  cursor: pointer;
-  background: #FF0000;
-  color: #fff;
-}
+  .header
+  {
+    text-align: center;
+    height: 80px;
+    background: black;
+    color: #fff;
+    font-weight: bold;
+    line-height: 80px;
+  }
+  .form-content
+  {
+    padding: 5%;
+    border: 1px solid #ced4da;
+    margin-bottom: 2%;
+  }
+  .form-control{
+    border-radius:1.5rem;
+  }
+  .btnSubmit
+  {
+    border:none;
+    border-radius:1.5rem;
+    padding: 1%;
+    width: 100%;
+    cursor: pointer;
+    background: black;
+    color: #fff;
+  }
+  .btnFacebook
+  {
+    border:none;
+    border-radius:1.5rem;
+    padding: 1%;
+    width: 100%;
+    cursor: pointer;
+    background: #0000FF;
+    color: #fff;
+  }
+  .btnGoogle
+  {
+    border:none;
+    border-radius:1.5rem;
+    padding: 1%;
+    width: 100%;
+    cursor: pointer;
+    background: #FF0000;
+    color: #fff;
+  }
+  button {
+    outline: none;
+  }
+  .invalidField{
+    border-color:#dc3545;
+  }
   `],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class AuthLoginComponent {
+export class AuthLoginComponent implements OnInit{
 
   loginUserForm: FormGroup;
   userNotValid: boolean;
+  socialMedia: boolean;
+  validatedForm: boolean;
 
   constructor(
     private authenticationService: AuthenticationService,
-    @Inject(PLATFORM_ID) private platformId,
-    private router: Router
+    private router: Router,
   ) {
+    this.userNotValid = false;
+    this.socialMedia = false;
+    this.validatedForm = false;
+  }
+
+  ngOnInit() {
     this.loginUserForm = new FormGroup({
       'email': new FormControl('', [Validators.required]),
       'password': new FormControl('', [Validators.required])
     });
-    this.userNotValid = false;
   }
 
-  ngOnInit() { }
-
   loginUser() {
-    this.authenticationService.authWithEmail(this.loginUserForm.value)
-    .then((response: any) => {
-      response.user.getIdTokenResult().then((res: any) => {
-        if (isPlatformBrowser(this.platformId)) {
-          localStorage.setItem('User', JSON.stringify({
-            uid: response.user.uid,
+    if (this.loginUserForm.valid) {
+      this.authenticationService.loginUser(this.loginUserForm.value)
+        .then((response: any) => {
+        response.user.getIdTokenResult().then((res: any) => {
+          this.authenticationService.saveCurrentUSer({
             user: response.user.email.split('@', 1)[0],
             email: response.user.email,
             refresh_token: response.user.refreshToken,
             token: res.token
-          }));
+          });
           this.router.navigate(['/']);
-        }
-      });
-    }).catch(error => this.userNotValid = true);   }
+        });
+        }).catch(error => this.userNotValid = true);
+    } else {
+      this.validatedForm = true;
+    }
+  }
 
   signInSocialMedia(socialMedia: boolean) {
     if (socialMedia) {
       this.authenticationService.authWithFacebook().then((response: any) => {
         response.user.getIdTokenResult().then((res: any) => {
-          if (isPlatformBrowser(this.platformId)) {
-            localStorage.setItem('User', JSON.stringify({
-              user: response.user.email.split('@', 1)[0],
-              email: response.user.email,
-              refresh_token: response.user.refreshToken,
-              token: res.token
-            }));
-          }
+          this.authenticationService.saveCurrentUSer({
+            user: response.user.email.split('@', 1)[0],
+            email: response.user.email,
+            refresh_token: response.user.refreshToken,
+            token: res.token
+          });
         }).then(() => {
-          this.authenticationService.currentUser.subscribe((user: any) => {
-          })
-          // this.router.navigate(['/']);
+          this.router.navigate(['/']);
         });
       }).catch(error => this.userNotValid = true);
     } else {
       this.authenticationService.authWithGoogle().then((response: any) =>  {
         response.user.getIdTokenResult().then((res: any) => {
-          if (isPlatformBrowser(this.platformId)) {
-            localStorage.setItem('User', JSON.stringify({
-              user: response.user.email.split('@', 1)[0],
-              email: response.user.email,
-              refresh_token: response.user.refreshToken,
-              token: res.token
-            }));
-          }
+          this.authenticationService.saveCurrentUSer({
+            user: response.user.email.split('@', 1)[0],
+            email: response.user.email,
+            refresh_token: response.user.refreshToken,
+            token: res.token
+          });
         }).then(() => {
           this.router.navigate(['/']);
         });
       }).catch(error => this.userNotValid = true);
     }
   }
-}
 
+}
